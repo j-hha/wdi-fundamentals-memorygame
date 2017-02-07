5ed5f9b8aaff6ada43de1879512f03b129d7e60e
j-hha <j-hha@bla.bla>
2017-02-06 19:30:14 -0500

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,17 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "queen",
+    cardTwo = "queen",
+    cardThree = "king",
+    cardFour = "king";
+
+/* testing for equality:
+       - comparing cardOne and cardTwo
+       - both have the value "queen"
+       --> this should produce 'true' */
+    console.log(cardOne == cardTwo);
+
+    /* - comparing cardOne and cardThree
+       - cardOne = "queen", cardThree = "king"
+       --> this should produce 'false' */
+    console.log(cardOne == cardThree);
