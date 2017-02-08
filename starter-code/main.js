04bd80d4da399f79914d3e1d12143262114bece0
j-hha <j-hha@bla.bla>
2017-02-08 09:21:54 -0500

    added game logic and card images

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,20 +1,67 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen",
-    cardTwo = "queen",
-    cardThree = "king",
-    cardFour = "king";
+var cards = [],
+    cardsInPlay = [];
 
-var createCards = function() {
-  for(var i=0; i<4; i++) {
-    var gameBoard = document.getElementById("game-board"),
-        newDiv = document.createElement("div");
-        newDiv.className = "card"
-        gameBoard.appendChild(newDiv);
-    }
-  };
+    cards = ["queen", "queen", "king", "king"];
 
-  createCards();
+    createCards = function() {
+      for(var i=0, length=cards.length; i<length; i++) {
+        var gameBoard = document.getElementById("game-board"),
+            newCard = document.createElement("div");
+            newCard.className = "card";
+            newCard.setAttribute("data-card", cards[i]);
+            newCard.addEventListener("click", isTwoCards);
+            gameBoard.appendChild(newCard);
+            console.log(newCard);
+
+          //  var cardElement = gameBoard.querySelectorAll('div');
+            // console.log(cardElement);
+            //cardElement.addEventListener("click", isTwoCards);
+          }
+        };
+
+        createCards();
+
+        isMatch = function() {
+          if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("You found a match!");
+          }
+          else {
+            alert("Sorry, try again.");
+          }
+        };
+
+        //checks to see if there are cards in play
+      function isTwoCards() {
+        // add card to array of cards in play
+        // 'this' hasn't been covered in this pre-work, but
+        // for now, just know it gives you access to the card the user clicked on
+        cardsInPlay.push(this.getAttribute('data-card'));
+
+        // if you have two cards in play, check for a match
+        if (cardsInPlay.length === 2) {
+
+          // pass the cardsInPlay as an argument to the isMatch function
+          isMatch(cardsInPlay);
+
+          // clear cards in play array for your next try
+          cardsInPlay = [];
+        }
+
+        if (this.getAttribute('data-card') == 'king') {
+          this.innerHTML = '<img src="spades-884197_640.png" alt="King of Spades" />'
+          this.style.backgroundColor = 'transparent';
+          this.style.border = '0';
+        }
+        else {
+          this.innerHTML = '<img src="hearts-884201_640.png" alt="Queen of Hearts" />'
+          this.style.backgroundColor = 'transparent';
+          this.style.border = '0';
+        }
+      }
+
+
 
 /* testing for equality:
 - get value of first card
@@ -25,12 +72,12 @@ var createCards = function() {
 
        - comparing cardOne and cardTwo
        - both have the value "queen"
-       --> this should produce 'true' */
+       --> this should produce 'true'
     console.log(cardOne == cardTwo);
 
-    /* - comparing cardOne and cardThree
+    - comparing cardOne and cardThree
        - cardOne = "queen", cardThree = "king"
-       --> this should produce 'false' */
+       --> this should produce 'false'
     console.log(cardOne == cardThree);
 
     if (cardOne === cardFour) {
@@ -46,3 +93,4 @@ var createCards = function() {
     else {
       alert("Sorry, try again.");
     }
+*/
