1df7b320ddd0d063b9fdf35e5273ff09cadea0d7
j-hha <j-hha@bla.bla>
2017-02-09 21:02:15 -0500

    clean up

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,109 +1,102 @@
 //Game logic
-var cards = [],
+var cards = ["queen", "king", "queen", "king"],
     cardsInPlay = [],
-    currentScore = document.getElementById("currentScore"),
+    tallyElement = document.getElementById("tallyElement"),
     score = 0,
-    reset = document.getElementById("reset"),
+    resetButton = document.getElementById("resetButton"),
     gameBoard = document.getElementById("game-board");
 
-    cards = ["queen", "king", "queen", "king"];
-
 //creates cards on gameBoard
-    createCards = function() {
-      //for reset button: this resets gameboard and score
-      gameBoard.innerHTML = "";
-      score = 0;
-      currentScore.innerHTML = score;
-      //creates cards on gameBoard
-      for(var i=0, length=cards.length; i<length; i++) {
-        var newCard = document.createElement("div");
-            newCard.className = "card";
-            newCard.setAttribute("data-card", cards[i]);
-            newCard.addEventListener("click", isTwoCards);
-            gameBoard.appendChild(newCard);
-          }
-        };
+var createCards = function() {
+  //this function is also used to reset the gameBoard and score
+  //when the resetButton is clicked
+  gameBoard.innerHTML = "";
+  score = 0;
+  tallyElement.innerHTML = score;
+  //creates cards on gameBoard
+  for(var i=0, length=cards.length; i<length; i++) {
+    var newCard = document.createElement("div");
+    newCard.className = "card";
+    newCard.setAttribute("data-card", cards[i]);
+    newCard.addEventListener("click", isTwoCards);
+    gameBoard.appendChild(newCard);
+  }
+};
 
-        createCards();
+//compares two cards
+ var isMatch = function() {
+   // if clicked cards have the same value corresponding alert message
+   // is displayed and score is updated
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+     score += 1;
+     tallyElement.innerHTML = score;
+     alert("You found a match! Your current score is " + score);
+   }
+   // else: clicked cards do not have the same value & corresponding alert
+   // message is displayed
+   else {
+     alert("Sorry, no match. Flip cards back over and then try again.");
+   }
 
-        //compares two cards
-        isMatch = function() {
-          // if clicked cards have the same value corresponding alert message
-          // is displayed and score is updated
-          if (cardsInPlay[0] === cardsInPlay[1]) {
-            score += 1;
-            alert("You found a match! Your current score is " + score);
-            currentScore.innerHTML = score;
-          }
-          // else: clicked cards do not have the same value & corresponding alert
-          // message is displayed
-          else {
-            alert("Sorry, no match. Flip cards back over and then try again.");
-          }
+   /* This code is intended to reset all cards automatically after isMatch() has
+      run - BUT I decided to use a different solution (cards have to be turned
+      over 'manually' by clicking) since running this code in combination with the
+      alert box will result in the second card's front side never being displayed.
 
-          /*To reset all cards automatically after isMatch() has run - BUT:
-          I decided to use a different version (cards have to be turned
-          over 'manually' by clicking) since this would prevent the
-          second card from being displayed by the alert box:
+      var allCards = document.getElementsByClassName('card');
+      for(j=0; j<allCards.length; j++) {
+        //resets attributes to match styles for back of card
+        allCards[j].innerHTML = '';
+        allCards[j].style.backgroundColor = '#FFFF00';
+        allCards[j].style.border = '1px solid rgb(0,0,0)';
+        allCards[j].setAttribute('clicked', 'false');
+      } */
+    };
 
-          allCards = document.getElementsByClassName('card');
-          for(j=0; j<allCards.length; j++) {
-            //resets attributes to match styles for back of card
-            allCards[j].innerHTML = ''
-            allCards[j].style.backgroundColor = '#FFFF00';
-            allCards[j].style.border = '1px solid rgb(0,0,0)';
-            allCards[j].setAttribute('clicked', 'false');
-          }*/
-        };
-
-
-        //checks to see if there are cards in play
-      function isTwoCards() {
-        //Checks value of card, displays corresponding pic and clears styles for back of card)
-        if (this.getAttribute('data-card') === 'king') {
-          this.innerHTML = '<img src="spades-884197_640.png" alt="King of Spades" />'
-          this.style.backgroundColor = 'transparent';
-          this.style.border = '0';
-        }
-        else {
-          this.innerHTML = '<img src="hearts-884201_640.png" alt="Queen of Hearts" />'
-          this.style.backgroundColor = 'transparent';
-          this.style.border = '0';
-        }
-        /* conditional prevents false "found match" message when same card is
-        beeing clicked twice in a row and instead flips card to backside again */
-        if (this.getAttribute('clicked') === 'true') {
-          // would be used in combination with the automatical reset version:
-          // return;
-          //resets attributes to match styles for back of card
-          this.setAttribute('clicked', 'false');
-          this.innerHTML = '';
-          this.style.backgroundColor = '#FFFF00';
-          this.style.border = '1px solid rgb(0,0,0)';
-          cardsInPlay = [];
-        }
-
-        else {
-          //sets a clicked = true attribute for clicked card
-          this.setAttribute('clicked', 'true');
-          // add card to array of cards in play
-          // 'this' hasn't been covered in this pre-work, but
-          // for now, just know it gives you access to the card the user clicked on
-          cardsInPlay.push(this.getAttribute('data-card'));
-
-          // if you have two cards in play, check for a match
-          if (cardsInPlay.length === 2) {
-
-            // pass the cardsInPlay as an argument to the isMatch function
-            isMatch(cardsInPlay);
-
-
-            // clear cards in play array for your next try
-            cardsInPlay = [];
-          }
-        }
+    //checks to see if there are cards in play
+    var isTwoCards = function() {
+      //Checks value of card, displays corresponding pic)
+      if (this.getAttribute('data-card') === 'king') {
+        this.innerHTML = '<img src="spades-884197_640.png" alt="King of Spades" />'
+        this.className = "card front";
+      }
+      else {
+        this.innerHTML = '<img src="hearts-884201_640.png" alt="Queen of Hearts" />'
+        this.className = "card front";
       }
 
-      //resets whole gameBoard and score tally
+      /* conditional prevents false "found match" message when same card is
+      being clicked twice in a row and instead flips card to backside again */
+      if (this.getAttribute('clicked') === 'true') {
+        /* would be used in combination with the automatic reset version:
+        return; */
+        //resets attributes to match styles for back of card
+        this.setAttribute('clicked', 'false');
+        this.innerHTML = '';
+        this.className = "card";
+        // removes last card from cardsInPlay for new try
+        cardsInPlay.pop();
+      }
 
-      reset.addEventListener("click", createCards);
+      else {
+        //sets clicked = true attribute for clicked card
+        this.setAttribute('clicked', 'true');
+        // add card to array of cards in play
+        // 'this' hasn't been covered in this pre-work, but
+        // for now, just know it gives you access to the card the user clicked on
+        cardsInPlay.push(this.getAttribute('data-card'));
+        // if you have two cards in play, check for a match
+        if (cardsInPlay.length === 2) {
+          // pass the cardsInPlay as an argument to the isMatch function
+          isMatch(cardsInPlay);
+          // clear cards in play array for next try
+          cardsInPlay = [];
+        }
+      }
+    };
+
+    // calls the function creating the cards on gameBoard
+    createCards();
+
+    //resets whole gameBoard and score tally
+    resetButton.addEventListener("click", createCards);
